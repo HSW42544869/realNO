c6ac109d4ba52eae29bebea79b44225e7eadb3c9
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-29 02:46:46 +0800

    5/29 2.

---
+++ Real_Nightmare_Online/Assets/text/plsyermovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plsyermovement : MonoBehaviour
+{
+    public float movespeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}
