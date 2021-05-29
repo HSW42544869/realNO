eccc5bb9ae2564f56cbcbb129504b1f75b004ff8
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-29 22:08:02 +0800

    5/29 22.

--- Real_Nightmare_Online/Assets/text/plsyermovement.cs
+++ Real_Nightmare_Online/Assets/text/plsyermovement.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class plsyermovement : MonoBehaviour
 {
     public float movespeed = 5f;
+    public float dashAmount = 50f;
 
     public Rigidbody2D rb;
     public Animator ani;
+    public BoxCollider2D Box;
+    private bool isDashButtonDown;
 
     Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    { 
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         ani.SetFloat("Horizontal", movement.x);
         ani.SetFloat("Vertical", movement.y);
         ani.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDashButtonDown = true;
+        }
     }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+
+        if (isDashButtonDown)
+        {
+            
+            rb.MovePosition(transform.position + movement * movespeed * dashAmount);
+            isDashButtonDown = false;
+        }
     }
 }
