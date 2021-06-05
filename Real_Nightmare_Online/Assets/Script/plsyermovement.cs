5396702d0ed4abe3294acbd870ffd7bfab3876eb
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-06-05 20:34:26 +0800

    6/5 20.

--- Real_Nightmare_Online/Assets/Script/plsyermovement.cs
+++ Real_Nightmare_Online/Assets/Script/plsyermovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class plsyermovement : MonoBehaviour
 {
-    public float movespeed = 5f;    //速度
+    public float movespeed = 3f;    //速度
     
 
     [SerializeField] private LayerMask dashLayerMask;   //設定快速移動且不會被穿越
@@ -20,6 +20,8 @@ public class plsyermovement : MonoBehaviour
     private Vector2 movement; //移動2維向量
     private Vector3 moveDir;  //瞬移3維向量
     private Vector3 rolldir;  //翻滾3為向量
+    private Vector3 lastMoveDir;
+    private float rollspeed;    //滾動速度
     private State state;
     private enum State { 
         Normal,
@@ -33,47 +35,75 @@ public class plsyermovement : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+       
         Move();
     }
     /// <summary>
     /// 正常移動    且用Blead tree判斷方向
     /// </summary>
     private void Move()     //且用Blend tree判斷方位
-    { 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+    {
+        switch (state) {
+            case State.Normal:
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = Input.GetAxisRaw("Vertical");
 
-        ani.SetFloat("Horizontal", movement.x);
-        ani.SetFloat("Vertical", movement.y);
-        ani.SetFloat("Speed", movement.sqrMagnitude);
+                ani.SetFloat("Horizontal", movement.x);
+                ani.SetFloat("Vertical", movement.y);
+                ani.SetFloat("Speed", movement.sqrMagnitude);
 
-        moveDir = new Vector3(movement.x, movement.y).normalized;
+                moveDir = new Vector3(movement.x, movement.y).normalized;
+                if (movement.x != 0 || movement.y != 0) {
+                    //Not idle
+                    lastMoveDir = moveDir;
+                }
 
-        if (Input.GetKeyDown(KeyCode.F))    //如果按下空白鍵觸發快速移動
-        {
-            isDashButtonDown = true;    //開關打開
-        }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            rolldir = moveDir;
+                if (Input.GetKeyDown(KeyCode.N))    //如果按下空白鍵觸發快速移動
+                {
+                    isDashButtonDown = true;    //開關打開
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    rolldir = lastMoveDir;
+                    rollspeed = 25f;
+                    state = State.Rolling;
+                }
+                break;
+            case State.Rolling:
+                float rollSpeedDropMutiplier = 3F;
+                rollspeed -= rollspeed * rollSpeedDropMutiplier * Time.deltaTime;
+
+                float rollSpeedMinimum = 10f;
+                if (rollspeed < rollSpeedMinimum) {
+                    state = State.Normal;
+                }
+                break;
         }
     }
     private void FixedUpdate()
     {
-        rb.velocity = moveDir * movespeed;  //瞬移動
-
-        if (isDashButtonDown)   //如果(瞬移開關打開)
+        switch (state)
         {
-            float dashAmount = 5f;
-            Vector3 dashPosition = transform.position + moveDir * dashAmount;
-            
-            RaycastHit2D raycastHit2d = Physics2D.Raycast(transform.position, moveDir, dashAmount, dashLayerMask);  //判斷是否移動方位是否有障礙物
-            if (raycastHit2d.collider != null)
-            {
-                dashPosition = raycastHit2d.point;
-            }
-            rb.MovePosition(dashPosition);
-            isDashButtonDown = false;
-        } 
+            case State.Normal:
+                rb.velocity = moveDir * movespeed;  //瞬移動
+
+                if (isDashButtonDown)   //如果(瞬移開關打開)
+                {
+                    float dashAmount = 5f;
+                    Vector3 dashPosition = transform.position + lastMoveDir * dashAmount;
+
+                    RaycastHit2D raycastHit2d = Physics2D.Raycast(transform.position, lastMoveDir, dashAmount, dashLayerMask);  //判斷是否移動方位是否有障礙物
+                    if (raycastHit2d.collider != null)
+                    {
+                        dashPosition = raycastHit2d.point;
+                    }
+                    rb.MovePosition(dashPosition);
+                    isDashButtonDown = false;
+                }
+                break;
+            case State.Rolling:
+                rb.velocity = rolldir * rollspeed;
+                break;
+        }
     }
 }
