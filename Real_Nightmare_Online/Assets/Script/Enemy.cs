c888324b3d02dc9b01c257c5677155537e36d737
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-06-09 22:05:53 +0800

    6/9 22.

---
+++ Real_Nightmare_Online/Assets/Script/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動範圍")]
+    public float moverange = 5;
+    [Header("目標物件")]
+    public Transform target;
+    [Header("移動速度")]
+    public float speed = 0.5f;
+
+    private void Awake()
+    {
+        target = GameObject.Find("character").transform;
+    }
+    private void Update()
+    {
+        Move();
+    }
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        float dis = Vector3.Distance(target.position, transform.position);
+
+        if (dis < moverange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            Vector3 offset =transform.position - target.position;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward,offset);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        Gizmos.DrawSphere(transform.position, moverange);
+    }
+}
