ab18f5d36b863c9517a3afba6a0e8530be596b6b
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-06-11 03:03:12 +0800

    6/11 3.

--- Real_Nightmare_Online/Assets/Script/PlayerAimWeapon.cs
+++ Real_Nightmare_Online/Assets/Script/PlayerAimWeapon.cs
@@ -1,34 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
 
 public class PlayerAimWeapon : MonoBehaviour
 {
+    public event EventHandler<OnShootEventArgs> OnShoot;
+    public class OnShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;
+        private Transform aimGunEndPointTransform;
+        public Vector3 shootPosition;
+    }
+
     private Transform aimTransform;
+    private Transform aimGunEndPointTransform;
+    private Animator ani;
 
     private void Awake()
     {
         aimTransform = transform.Find("Aim");   //追蹤目標
+        ani =aimTransform.GetComponent<Animator>(); //動畫控制
+        aimGunEndPointTransform = aimTransform.Find("GunEndPointPosition");
     }
     private void Update()
     {
         HandleAiming();
-        Handleshoot();
+        Handleshooting();
     }
     private void HandleAiming()
     {
         Vector3 mousePosition =  UtilsClass.GetMouseWorldPosition(); //取得屬標位置
 
-        Vector3 aimDirection = (mousePosition - transform.position).normalized; //鼠標的方向
+        Vector3 aimDirection = (mousePosition - aimTransform.position).normalized; //鼠標的方向
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
     }
-    private void Handleshoot()
+    private void Handleshooting()
     {
-        if (Input.GetMouseButtonDown(0)) { 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+
+            ani.SetBool("shoot",true);
+            OnShoot.Invoke(this, new OnShootEventArgs
+            {
+                gunEndPointPosition = aimGunEndPointTransform.position,
+                shootPosition = mousePosition,
+            });
             
         }
+        else
+        {
+            ani.SetBool("shoot", false);
+        }
+                
     }
         
 }
---
+++ Real_Nightmare_Online/Assets/Script/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private PlayerAimWeapon playerAimWeapon;
+
+    private void Start()
+    {
+        playerAimWeapon.OnShoot += PlayerAimWeapon_OnShoot;
+    }
+    private void PlayerAimWeapon_OnShoot(object sender, PlayerAimWeapon.OnShootEventArgs e)
+    {
+        UtilsClass.ShakeCamera(1f, .2f);
+        WeaponTrace.Create(e.gunEndPointPosition, e.shootPosition);
+        Shoot_Flash.AddFlash(e.gunEndPointPosition);
+    }
+}
