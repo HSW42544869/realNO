33991b55da40742a4d339fc6f1321ed3f71aad34
Yushain <73380147+YutianGG@users.noreply.github.com>
2021-07-15 11:58:20 +0800

    7/15

--- Real_Nightmare_Online/Assets/Script/plsyermovement.cs
+++ Real_Nightmare_Online/Assets/Script/plsyermovement.cs
@@ -176,6 +176,15 @@ public class plsyermovement : MonoBehaviour
 
             SceneManager.LoadScene(lvIndex);                            // 載入下一關
         }
+        if(collision.name == "heal")
+        {
+            if (live + 15 > 100)
+            {
+                live = 100;
+                Destroy(collision.gameObject);
+            }
+            else live += 15; Destroy(collision.gameObject);
+        }
     }
     #endregion
 
